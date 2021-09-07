0b75edfd7f527cd4f97fe1f4bca5165417df8831
99058830 <99058830@mydavinci.nl>
2021-09-07 13:39:21 +0200

    person script done

---
+++ person.py
@@ -0,0 +1,15 @@
+name = input("Vul uw naam in.")
+adress = input("Vul uw adress in.")
+postal = input("Vul uw postcode in.")
+place = input("Vul uw woonplaats in.")
+
+
+
+
+
+print("----------------------------------------------------")
+print("|  Naam      :", name,"")
+print("|  Adres     :", adress,"")
+print("|  Postcode  :", postal,"")
+print("|  Woonplaats:", place,"")
+print("----------------------------------------------------")
